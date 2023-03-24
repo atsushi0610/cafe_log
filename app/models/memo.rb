071c63b2d7daec15c66f2a2156207f2cacd64596
atsushi0610 <kogure.bbc@gmail.com>
2023-03-24 21:45:21 +0900

    バリデーションの設定

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -5,4 +5,13 @@ class Memo < ApplicationRecord
   belongs_to :aroma
   belongs_to :roast
   belongs_to :drink
+
+  validates :title, :impression, :date, presence: true
+
+  validates :kind_id, numericality: { other_than: 1 } 
+  validates :taste_id, numericality: { other_than: 1 } 
+  validates :aroma_id, numericality: { other_than: 1 } 
+  validates :roast_id, numericality: { other_than: 1 } 
+  validates :drink_id, numericality: { other_than: 1 } 
+
 end
