a745c60f773cfb8239f59d39ce84a2a63c74413c
atsushi0610 <kogure.bbc@gmail.com>
2023-03-26 15:37:59 +0900

    deviseのビューの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   private
@@ -14,4 +13,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "memos#index"
   devise_for :users
+  root to: "memos#index"
   resources :memos
 end
