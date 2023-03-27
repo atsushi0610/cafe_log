be9eaab49a0eec469df09246674d85e18e08af5b
atsushi0610 <kogure.bbc@gmail.com>
2023-03-27 21:27:38 +0900

    コーヒーメモ投稿画面の作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,4 +1,9 @@
 class MemosController < ApplicationController
   def index
+    
+  end
+
+  def new
+    @memo = Memo.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "memos#index"
-  resources :memos
+  resources :memos, only: [:index, :new]
 end
