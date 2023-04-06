06eca2b3b80e1f62cf61ba29084407fc176935ea
atsushi0610 <kogure.bbc@gmail.com>
2023-04-06 18:44:10 +0900

    コメント削除機能、commentモデルの単体テストコード

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,18 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
-    redirect_to "/memos/#{comment.memo.id}"
+    redirect_to memo_path(comment.memo.id)
+  end
+
+  def destroy
+    @memo = Memo.find(params[:memo_id])
+    comment = @memo.comments.find(params[:id])
+    if current_user.id == comment.user.id
+      comment.destroy
+    redirect_to memo_path(comment.memo.id)
+    else
+      render "memos/show"
+    end
   end
 
   private
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメントの保存' do
+    context 'コメントが投稿できる場合' do
+      it 'コメントが入力されていれば投稿できる' do
+        expect(@comment).to be_valid
+      end
+    end
+    context 'commentが投稿できない場合' do
+      it 'コメントが空では投稿できない' do
+        @comment.comment = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("コメントを入力してください")
+      end
+      it 'ユーザーが紐づいていないと投稿できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Userを入力してください')
+      end
+      it 'コーヒーメモが紐づいていないと投稿できない' do
+        @comment.memo = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Memoを入力してください')
+      end
+    end
+  end
 end
