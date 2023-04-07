10a1d60c45b54cb1cca09f8f16e54494f7816576
atsushi0610 <kogure.bbc@gmail.com>
2023-04-07 21:19:44 +0900

    ユーザーマイページの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
-    @memos = current_user.memos
+    @memos = current_user.memos.order("created_at DESC")
   end
 end
